b530db896e3827d4f8d52cb4fd12ba8f9117fa23
omidng <omidn@ece.ubc.ca>
2014-12-20 19:27:46 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,16 +8,23 @@ makeCacheMatrix <- function(x = matrix()) {
         set <- function(y){
         x <<- y
         m <<- NULL
-}
-
-get <- function() x
-setmatrix <- function(solve) m <<- solve
-getmatrix <- function() m
-list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        matrix <- x$get()
+        m <- solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
