64c22b777be677e3f647ba0904bce7ba415de841
omidng <omidn@ece.ubc.ca>
2014-12-20 19:28:33 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,8 +6,8 @@
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y){
-        x <<- y
-        m <<- NULL
+                x <<- y
+                m <<- NULL
         }
         get <- function() x
         setmatrix <- function(solve) m <<- solve
